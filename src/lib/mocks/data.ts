6e1b5d27bac8ffd3f5626dd7ba065ee0e9c9662f
Alfredo Guida <guida.alfredo@gmail.com>
2025-11-24 19:23:50 +0100

    base components

---
+++ src/lib/mocks/data.ts
@@ -0,0 +1,61 @@
+export const categories = [
+	{
+		id: '1',
+		name: "Leadership",
+	},
+	{
+		id: '2',
+		name: "Productivity",
+	},
+	{
+		id: '3',
+		name: "Communication",
+	},
+	{
+		id: '4',
+		name: "Creativity",
+	},
+	{
+		id: '5',
+		name: "Wellness",
+	},
+]
+
+export const languages = [
+	{
+		code: 'en',
+		name: 'English'
+	},
+	{
+		code: 'es',
+		name: 'Spanish'
+	},
+	{
+		code: 'fr',
+		name: 'French'
+	},
+	{
+		code: 'it',
+		name: 'Italian',
+	},
+]
+
+
+export const providers = [
+	{
+		id:1,
+		name:'Linkedin'
+	},
+	{
+		id:2,
+		name:'Mentor'
+	},
+	{
+		id:3,
+		name:"Pack"
+	},
+	{
+		id:4,
+		name:"Skilla"
+	}
+]
